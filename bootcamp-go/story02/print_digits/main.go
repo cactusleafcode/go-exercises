236c60698520f824c51a55c4dde32cc6d54cec7e
Your Name <you@example.com>
2025-06-18 00:14:52 +0500

    handshake to printdigits

---
+++ bootcamp-go/story02/print_digits/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "github.com/alem-platform/ap"
+
+func main() {
+	ap.PutRune('0')
+	ap.PutRune('1')
+	ap.PutRune('2')
+	ap.PutRune('3')
+	ap.PutRune('4')
+	ap.PutRune('5')
+	ap.PutRune('6')
+	ap.PutRune('6')
+	ap.PutRune('7')
+	ap.PutRune('8')
+	ap.PutRune('9')
+	ap.PutRune('\n')
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/cactusleafcode/go-exercises
+
+go 1.24.4
+
+require github.com/alem-platform/ap v0.1.3
